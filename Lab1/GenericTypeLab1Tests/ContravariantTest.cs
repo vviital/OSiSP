dd29cd7acbdaa9551203e12a41eab790e5324f4e
Zanevski Vitali <vviital@gmail.com>
2015-04-13 12:48:01 +0300

    Unit test
    first try

---
+++ Lab1/GenericTypeLab1Tests/ContravariantTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Configuration;
+using Lab1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenericTypeLab1Tests
+{
+    [TestClass]
+    public class ContravariantTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IInformation<House> houseInformation = new HouseInformation();
+            IInformation<Mansion> mansionInformation = new MansionInformation();
+
+            bool isGood = true;
+
+            try
+            {
+                mansionInformation = (IInformation<Mansion>)houseInformation;
+            }
+            catch (Exception exception)
+            {
+                isGood = false;
+            }
+
+            Assert.AreEqual(true, isGood, "Interface IInformation<T> isn't contravariant");
+        }
+    }
+}
---
+++ Lab1/GenericTypeLab1Tests/CovariantTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenericTypeLab1Tests
+{
+    using Lab1;
+
+    [TestClass]
+    public class CovariantTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IBuilder<House> houseBuilder = new HouseBuilder();
+            IBuilder<Mansion> mansionBuilder = new MansionBuilder();
+
+            bool isGood = true;
+
+            try
+            {
+                houseBuilder = (IBuilder<House>) mansionBuilder;
+            }
+            catch (Exception exception)
+            {
+                isGood = false;
+            }
+
+            Assert.AreEqual(true, isGood, "Interface IBuilder<T> isn't covariant");
+        }
+    }
+}
