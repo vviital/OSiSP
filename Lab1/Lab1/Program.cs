853f519e481c9c02c238ba068cad5f2cd0578086
Zanevski Vitali <vviital@gmail.com>
2015-04-13 09:49:39 +0300

    covariance
    step two

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -11,7 +11,17 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-
+            IBuilder<House> houseBuilder = new HouseBuilder();
+            IBuilder<Mansion> mansionBuilder = new MansionBuilder();
+            try
+            {
+                houseBuilder = (IBuilder<House>) mansionBuilder;
+                Console.WriteLine("Covariant interface IBuilder<T>");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Error!!!");
+            }
         }
     }
 }
