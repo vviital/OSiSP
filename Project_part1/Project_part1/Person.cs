fac6fb788db099227cab319f8eced6d14fd9340f
Zanevski Vitali <vviital@gmail.com>
2015-05-02 20:04:54 +0300

    Project. Part1. Subject Area

---
+++ Project_part1/Project_part1/Person.cs
@@ -0,0 +1,137 @@
+﻿namespace Project_part1
+{
+    /// <summary>
+    /// Class which represent a person.
+    /// </summary>
+    class Person
+    {
+        /// <summary>
+        /// The login
+        /// </summary>
+        protected string _login;
+
+        /// <summary>
+        /// The password
+        /// </summary>
+        protected string _password;
+
+        /// <summary>
+        /// The surname
+        /// </summary>
+        protected string _surname;
+
+        /// <summary>
+        /// The first name
+        /// </summary>
+        protected string _firstName;
+
+        /// <summary>
+        /// The phone number
+        /// </summary>
+        protected string _phoneNumber;
+
+        /// <summary>
+        /// The email
+        /// </summary>
+        protected string _email;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Person"/> class.
+        /// </summary>
+        public Person()
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Person"/> class.
+        /// </summary>
+        /// <param name="login">The login.</param>
+        /// <param name="password">The password.</param>
+        /// <param name="surname">The surname.</param>
+        /// <param name="firstName">The first name.</param>
+        /// <param name="phoneNumber">The phone number.</param>
+        /// <param name="email">The email.</param>
+        public Person(string login, string password, string surname, string firstName, string phoneNumber, string email)
+        {
+            this.Login = login;
+            this.Password = password;
+            this.Surname = surname;
+            this.FirstName = firstName;
+            this.PhoneNumber = phoneNumber;
+            this.Email = email;
+        }
+
+        /// <summary>
+        /// Gets or sets the login.
+        /// </summary>
+        /// <value>
+        /// The login.
+        /// </value>
+        public string Login
+        {
+            get { return this._login; }
+            set { this._login = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        /// <value>
+        /// The password.
+        /// </value>
+        public string Password
+        {
+            get { return this._password; }
+            set { this._password = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the surname.
+        /// </summary>
+        /// <value>
+        /// The surname.
+        /// </value>
+        public string Surname
+        {
+            get { return this._surname; }
+            set { this._surname = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        /// <value>
+        /// The first name.
+        /// </value>
+        public string FirstName
+        {
+            get { return this._firstName; }
+            set { this._firstName = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the phone number.
+        /// </summary>
+        /// <value>
+        /// The phone number.
+        /// </value>
+        public string PhoneNumber
+        {
+            get { return this._phoneNumber; }
+            set { this._phoneNumber = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
+        public string Email
+        {
+            get { return this._email; }
+            set { this._email = value; }
+        }
+    }
+}
---
+++ Project_part1/Project_part1/Player.cs
@@ -0,0 +1,132 @@
+﻿namespace Project_part1
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Class Player.
+    /// </summary>
+    class Player : Person
+    {
+        /// <summary>
+        /// The verify flag
+        /// </summary>
+        protected bool _verifyFlag;
+
+        /// <summary>
+        /// The passport information
+        /// </summary>
+        protected PassportInformation _passportInformation;
+
+        /// <summary>
+        /// The account balance
+        /// </summary>
+        protected double _accountBalance;
+
+        /// <summary>
+        /// The friends
+        /// </summary>
+        protected List<Friend> _friends;
+ 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Player"/> class.
+        /// </summary>
+        public Player()
+            : base()
+        {
+            this.Friends = new List<Friend>();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Player"/> class.
+        /// </summary>
+        /// <param name="login">The login.</param>
+        /// <param name="password">The password.</param>
+        /// <param name="surname">The surname.</param>
+        /// <param name="firstName">The first name.</param>
+        /// <param name="phoneNumber">The phone number.</param>
+        /// <param name="email">The email.</param>
+        /// <param name="verifyFlag">if set to <c>true</c> [verify flag].</param>
+        /// <param name="passport">The passport.</param>
+        /// <param name="accountBalance">The account balance.</param>
+        public Player(string login, string password, string surname, string firstName, string phoneNumber, string email,
+            bool verifyFlag, PassportInformation passport, double accountBalance)
+            : base(login, password, surname, firstName, phoneNumber, email)
+        {
+            this.VerifyFlag = verifyFlag;
+            this.Passport = passport;
+            this.AccountBalance = accountBalance;
+            this.Friends = new List<Friend>();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Player"/> class.
+        /// </summary>
+        /// <param name="login">The login.</param>
+        /// <param name="password">The password.</param>
+        /// <param name="surname">The surname.</param>
+        /// <param name="firstName">The first name.</param>
+        /// <param name="phoneNumber">The phone number.</param>
+        /// <param name="email">The email.</param>
+        /// <param name="verifyFlag">if set to <c>true</c> [verify flag].</param>
+        /// <param name="passport">The passport.</param>
+        /// <param name="accountBalance">The account balance.</param>
+        /// <param name="friends">The friends.</param>
+        public Player(string login, string password, string surname, string firstName, string phoneNumber, string email,
+           bool verifyFlag, PassportInformation passport, double accountBalance, List<Friend> friends)
+            : base(login, password, surname, firstName, phoneNumber, email)
+        {
+            this.VerifyFlag = verifyFlag;
+            this.Passport = passport;
+            this.AccountBalance = accountBalance;
+            this.Friends = friends;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether [verify flag].
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if [verify flag]; otherwise, <c>false</c>.
+        /// </value>
+        public bool VerifyFlag
+        {
+            get { return this._verifyFlag; }
+            set { this._verifyFlag = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the passport information.
+        /// </summary>
+        /// <value>
+        /// The passport information.
+        /// </value>
+        public PassportInformation Passport
+        {
+            get { return this._passportInformation; }
+            set { this._passportInformation = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the account balance.
+        /// </summary>
+        /// <value>
+        /// The account balance.
+        /// </value>
+        public double AccountBalance
+        {
+            get { return this._accountBalance; }
+            set { this._accountBalance = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the friends.
+        /// </summary>
+        /// <value>
+        /// The friends.
+        /// </value>
+        public List<Friend> Friends
+        {
+            get { return this._friends; }
+            set { this._friends = value; }
+        } 
+    }
+}
