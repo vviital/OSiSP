f194122dabf6588ba6b64d0d939604ed96aead48
Zanevski Vitali <vviital@gmail.com>
2015-05-14 11:14:46 +0300

    subject area. end

---
+++ Project_part1/Project_part1/Administrator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_part1
+{
+    class Administrator : Person
+    {
+        public Administrator()
+            : base()
+        {
+            
+        }
+
+        public Administrator(string login, string password, string surname, string firstname, bool sex, string email,
+            DateTime birth, Country homeCountry)
+            : base(login, password, surname, firstname, sex, email, birth, homeCountry)
+        {
+            
+        }
+    }
+}
---
+++ Project_part1/Project_part1/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_part1
+{
+    class Player : Person
+    {
+        public string PhoneNumber { get; set; }
+
+        public double AccountBalance { get; set; }
+
+        public bool VerifyFlag { get; set; }
+
+        public List<Player> Friends { get; set; }
+ 
+        public Player()
+        {
+            
+        }
+
+        public Player(string login, string password, string surname, string firstname, bool sex, string email,
+            DateTime birth, Country country, string phoneNumber)
+            : base(login, password, surname, firstname, sex, email, birth, country)
+        {
+            this.PhoneNumber = phoneNumber;
+            this.AccountBalance = 0;
+            this.VerifyFlag = false;
+        }
+
+        public Player(string login, string password, string surname, string firstname, bool sex, string email,
+            DateTime birth, Country country, string phoneNumber, double accountBalance, bool verifyFlag)
+            : base(login, password, surname, firstname, sex, email, birth, country)
+        {
+            this.PhoneNumber = phoneNumber;
+            this.AccountBalance = accountBalance;
+            this.VerifyFlag = verifyFlag;
+        }
+    }
+}
