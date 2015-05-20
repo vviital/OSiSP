bedf43e8f200deeb254c1ff5e4b23c53858d6105
Zanevski Vitali <vviital@gmail.com>
2015-05-20 17:09:52 +0300

    CourceWork

---
+++ Курсовая/SocietyAnalysisWeb/SocietyAnalysisWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SocietyAnalysisWeb.Startup))]
+namespace SocietyAnalysisWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
